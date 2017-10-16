8832054064ec7994e25db13c531e241c9fa85e11
Hengki Sihombing <hengki.sihombing@gmail.com>
2017-10-16 16:20:47 +0700

    knowledge base

--- app.js
+++ app.js
@@ -2,18 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const builder = require('botbuilder')
 const fs = require('fs')
-const ticketsApi = require('./ticketsApi')
 const bodyParser = require('body-parser')
 const methodOverride = require('method-override')
 const restify = require('restify')
+
+const ticketsApi = require('./ticketsApi')
+const azureSearch = require('./azureSearchApiClient')
 
 const server = express()
 
 const listenPort = process.env.port || process.env.PORT || 3978
 const ticketSubmissionUrl = process.env.TICKET_SUBMISSION_URL || `http://localhost:${listenPort}`
 
-server.listen(listenPort, () => {
-  console.log('%s listening to %s', server.name, process.env.PORT)
+const azureSearchQuery = azureSearch({
+  searchName: process.env.AZURE_SEARCH_ACCOUNT,
+  indexName: process.env.AZURE_SEARCH_INDEX,
+  searchKey: process.env.AZURE_SEARCH_KEY
 })
 
 server.use(bodyParser.json({limit: '15mb'}))
@@ -52,46 +56,45 @@ bot.dialog('Help',
 // Receive messages from the user and respond by echoing each message back (prefixed with 'You said:')
 bot.dialog('SubmitTicket', [
   (session, args, next) => {
-      var category = builder.EntityRecognizer.findEntity(args.intent.entities, 'category');
-      var severity = builder.EntityRecognizer.findEntity(args.intent.entities, 'severity');
+      var category = builder.EntityRecognizer.findEntity(args.intent.entities, 'category')
 
       if (category && category.resolution.values.length > 0) {
-          session.dialogData.category = category.resolution.values[0];
+          session.dialogData.category = category.resolution.values[0]
       }
 
       if (severity && severity.resolution.values.length > 0) {
-          session.dialogData.severity = severity.resolution.values[0];
+          session.dialogData.severity = severity.resolution.values[0]
       }
 
-      session.dialogData.description = session.message.text;
+      session.dialogData.description = session.message.text
 
       if (!session.dialogData.severity) {
-          var choices = ['high', 'normal', 'low'];
-          builder.Prompts.choice(session, 'Which is the severity of this problem?', choices, { listStyle: builder.ListStyle.button });
+          var choices = ['high', 'normal', 'low']
+          builder.Prompts.choice(session, 'Which is the severity of this problem?', choices, { listStyle: builder.ListStyle.button })
       } else {
-          next();
+          next()
       }
   },
   (session, result, next) => {
       if (!session.dialogData.severity) {
-          session.dialogData.severity = result.response.entity;
+          session.dialogData.severity = result.response.entity
       }
 
       if (!session.dialogData.category) {
-          builder.Prompts.text(session, 'Which would be the category for this ticket (software, hardware, networking, security or other)?');
+          builder.Prompts.text(session, 'Which would be the category for this ticket (software, hardware, networking, security or other)?')
       } else {
-          next();
+          next()
       }
   },
   (session, result, next) => {
       if (!session.dialogData.category) {
-          session.dialogData.category = result.response;
+          session.dialogData.category = result.response
       }
 
       var message = `Great! I'm going to create a "${session.dialogData.severity}" severity ticket in the "${session.dialogData.category}" category. ` +
-                    `The description I will use is "${session.dialogData.description}". Can you please confirm that this information is correct?`;
+                    `The description I will use is "${session.dialogData.description}". Can you please confirm that this information is correct?`
 
-      builder.Prompts.confirm(session, message, { listStyle: builder.ListStyle.button });
+      builder.Prompts.confirm(session, message, { listStyle: builder.ListStyle.button })
   },
   (session, result, next) => {
     if (result.response) {
@@ -101,22 +104,22 @@ bot.dialog('SubmitTicket', [
             description: session.dialogData.description,
         }
 
-        const client = restify.createJsonClient({ url: ticketSubmissionUrl });
+        const client = restify.createJsonClient({ url: ticketSubmissionUrl })
 
         client.post('/api/tickets', data, (err, request, response, ticketId) => {
             if (err || ticketId == -1) {
-                session.send('Ooops! Something went wrong while I was saving your ticket. Please try again later.');
+                session.send('Ooops! Something went wrong while I was saving your ticket. Please try again later.')
             } else {
                 session.send(new builder.Message(session).addAttachment({
                     contentType: "application/vnd.microsoft.card.adaptive",
                     content: createCard(ticketId, data)
-                }));
+                }))
             }
 
-            session.endDialog();
-        });
+            session.endDialog()
+        })
     } else {
-        session.endDialog('Ok. The ticket was not created. You can start again if you want.');
+        session.endDialog('Ok. The ticket was not created. You can start again if you want.')
     }
   }
 ]).triggerAction({
@@ -133,3 +136,103 @@ const createCard = (ticketId, data) => {
 
   return JSON.parse(cardTxt)
 }
+
+server.listen(listenPort, () => {
+  console.log('%s listening to %s', server.name, process.env.PORT)
+})
+
+bot.dialog('ExploreKnowledgeBase', [
+  (session, args, next) => {
+      var category = builder.EntityRecognizer.findEntity(args.intent.entities, 'category')
+
+      if (!category) {
+          // retrieve facets
+          azureSearchQuery('facet=category', (error, result) => {
+              if (error) {
+                  session.endDialog('Ooops! Something went wrong while contacting Azure Search. Please try again later.')
+              } else {
+                  var choices = result['@search.facets'].category.map(item=> `${item.value} (${item.count})`)
+                  builder.Prompts.choice(session, 'Let\'s see if I can find something in the knowledge base for you. Which category is your question about?', choices, { listStyle: builder.ListStyle.button })
+              }
+          })
+      } else {
+          if (!session.dialogData.category) {
+              session.dialogData.category = category.entity
+          }
+
+          next()
+      }
+  },
+  (session, args) => {
+      var category
+
+      if (session.dialogData.category) {
+          category = session.dialogData.category
+      } else {
+          category = args.response.entity.replace(/\s\([^)]*\)/,'')
+      }
+      // search by category
+      azureSearchQuery('$filter=' + encodeURIComponent(`category eq '${category}'`), (error, result) => {
+          if (error) {
+              session.endDialog('Ooops! Something went wrong while contacting Azure Search. Please try again later.')
+          } else {
+              session.replaceDialog('ShowKBResults', { result, originalText: category })
+          }
+      })
+  }
+]).triggerAction({
+  matches: 'ExploreKnowledgeBase'
+})
+
+bot.dialog('DetailsOf', [
+  (session, args) => {
+      var title = session.message.text.substring('show me the article '.length)
+      azureSearchQuery('$filter=' + encodeURIComponent(`title eq '${title}'`), (error, result) => {
+          if (error || !result.value[0]) {
+              session.endDialog('Sorry, I could not find that article.')
+          } else {
+              session.endDialog(result.value[0].text)
+          }
+      })
+  }
+]).triggerAction({
+  matches: /^show me the article (.*)/i
+})
+
+bot.dialog('SearchKB', [
+  (session) => {
+      session.sendTyping()
+      azureSearchQuery(`search=${encodeURIComponent(session.message.text.substring('search about '.length))}`, (err, result) => {
+          if (err) {
+              session.send('Ooops! Something went wrong while contacting Azure Search. Please try again later.')
+              return
+          }
+          session.replaceDialog('ShowKBResults', { result, originalText: session.message.text })
+      })
+  }
+])
+.triggerAction({
+  matches: /^search about (.*)/i
+})
+
+bot.dialog('ShowKBResults', [
+  (session, args) => {
+      if (args.result.value.length > 0) {
+          var msg = new builder.Message(session).attachmentLayout(builder.AttachmentLayout.carousel)
+          args.result.value.forEach((faq, i) => {
+              msg.addAttachment(
+                  new builder.ThumbnailCard(session)
+                      .title(faq.title)
+                      .subtitle(`Category: ${faq.category} | Search Score: ${faq['@search.score']}`)
+                      .text(faq.text.substring(0, Math.min(faq.text.length, 50) + '...'))
+                      .images([builder.CardImage.create(session, 'https://raw.githubusercontent.com/GeekTrainer/help-desk-bot-lab/master/assets/botimages/head-smiling-medium.png')])
+                      .buttons([{ title: 'More details', value: `show me the article ${faq.title}`, type: 'postBack' }])
+              )
+          })
+          session.send(`These are some articles I\'ve found in the knowledge base for _'${args.originalText}'_, click **More details** to read the full article:`)
+          session.endDialog(msg)
+      } else {
+          session.endDialog(`Sorry, I could not find any results in the knowledge base for _'${args.originalText}'_`)
+      }
+  }
+])
